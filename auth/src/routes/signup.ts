bb8fd2e6f02ae112a2651660df27de1060c36981
lschulzes <lucassilvaiee@gmail.com>
2022-08-11 11:27:29 -0300

    feat: add validation for signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,25 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
-router.get(`/`, (req, res) => {
-  res.send("Hello World!");
-});
+router.post(
+  `/`,
+  [
+    body("email").isEmail().withMessage("Must be an valid email."),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).send(errors.array());
+
+    const { email, password } = req.body;
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
