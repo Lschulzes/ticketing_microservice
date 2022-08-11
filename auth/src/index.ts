ae67c19e614306787b20fd5731bcb23f0c5cdd99
lschulzes <lucassilvaiee@gmail.com>
2022-08-11 11:02:25 -0300

    feat: add auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import { json } from "body-parser";
+import { currentUserRouter } from "./routes/current-user";
+import {
+  CURRENT_USER_ENDPOINT,
+  SIGNIN_ENDPOINT,
+  SIGNOUT_ENDPOINT,
+  SIGNUP_ENDPOINT,
+} from "./resources/helpers";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 
 app.use(json());
 
-app.get("/api/users/:id", (req, res) => {
-  const { id } = req.params;
-
-  res.status(200).send(`User with id of ${id}`);
-});
+app.use(CURRENT_USER_ENDPOINT, currentUserRouter);
+app.use(SIGNIN_ENDPOINT, signinRouter);
+app.use(SIGNOUT_ENDPOINT, signoutRouter);
+app.use(SIGNUP_ENDPOINT, signupRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
---
+++ auth/src/resources/helpers.ts
@@ -0,0 +1,5 @@
+const API_ENDPOINT = "/api/users";
+export const CURRENT_USER_ENDPOINT = `${API_ENDPOINT}/current-user`;
+export const SIGNIN_ENDPOINT = `${API_ENDPOINT}/signin`;
+export const SIGNOUT_ENDPOINT = `${API_ENDPOINT}/signout`;
+export const SIGNUP_ENDPOINT = `${API_ENDPOINT}/signup`;
