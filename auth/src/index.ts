a5a41d9fb77c50ac51f84c69f27ec85ab95525b6
lschulzes <lucassilvaiee@gmail.com>
2022-08-11 09:37:35 -0300

    chore: add skaffold auth-depl auth-srv and ingress-srv config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,12 @@ const app = express();
 
 app.use(json());
 
+app.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.status(200).send(`User with id of ${id}`);
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
 });
