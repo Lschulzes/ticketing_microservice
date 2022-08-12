9f76f39debc0a786f0ea5e48bb527b646f23b34f
lschulzes <lucassilvaiee@gmail.com>
2022-08-12 13:49:41 -0300

    feat: add current-user endpoint

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
+import { User } from "./../models/User";
+import jwt from "jsonwebtoken";
 import { Router } from "express";
+import { AppError } from "../errors/app-error";
 
 const router = Router();
 
-router.get(`/`, (req, res) => {
-  res.send("Hello World!");
+router.get(`/`, async (req, res) => {
+  const sessionJWT = req.session?.jwt;
+  if (!sessionJWT) return res.send({ currentUser: null });
+
+  try {
+    const payload = jwt.verify(sessionJWT, process.env.JWT_KEY!) as {
+      id: string;
+    };
+
+    res.status(200).send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
