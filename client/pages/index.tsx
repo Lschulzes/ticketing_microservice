74691ae4afe18b4cbbb21bc332e89d5150e9f081
lschulzes <lucassilvaiee@gmail.com>
2022-08-14 21:51:35 -0300

    feat: add server side requests support through ingress-nginx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,23 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
+import buildClient from '../api/buildClient';
 
-const Home: NextPage = () => {
-  return <div className="btn btn-primary">Hello World!</div>;
+type PageProps = {
+  user: { id: string; email: string } | null;
+};
+
+const Home: NextPage<PageProps> = ({ user }: PageProps) => {
+  return <h3>Hello {user?.email}!</h3>;
+};
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async ({ req }) => {
+  try {
+    const client = buildClient(req);
+    const { data } = await client.get('/api/users/current-user');
+
+    return { props: { user: data.currentUser } };
+  } catch (error) {
+    return { props: { user: null } };
+  }
 };
 
 export default Home;
