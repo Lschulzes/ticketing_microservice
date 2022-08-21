7b282063b28060176c14e02bde70c1f31964dc72
lschulzes <lucassilvaiee@gmail.com>
2022-08-21 19:17:23 -0300

    chore: add NATS URL & CLUSTER_ID & CLIENT_ID envs

--- services/tickets/src/index.ts
+++ services/tickets/src/index.ts
@@ -9,8 +9,17 @@ const start = async () => {
   try {
     if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
     if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
+    if (!process.env.NATS_URL) throw new Error("NATS_URL must be defined");
+    if (!process.env.NATS_CLUSTER_ID)
+      throw new Error("NATS_CLUSTER_ID must be defined");
+    if (!process.env.NATS_CLIENT_ID)
+      throw new Error("NATS_CLIENT_ID must be defined");
 
-    await natsWrapper.connect("ticketing", "csscdd", "http://nats-srv:4222");
+    await natsWrapper.connect(
+      process.env.NATS_URL,
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID
+    );
 
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed!");
