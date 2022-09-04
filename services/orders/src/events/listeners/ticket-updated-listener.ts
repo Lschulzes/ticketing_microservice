48c9ce871210858701733e85e6dc6963a0a2f8d5
lschulzes <lucassilvaiee@gmail.com>
2022-09-04 13:28:05 -0300

    feat(services/orders): listen for tickets events

--- services/orders/src/events/listeners/ticket-updated-listener.ts
+++ services/orders/src/events/listeners/ticket-updated-listener.ts
@@ -3,9 +3,10 @@ import { Message } from "node-nats-streaming";
 import { Ticket } from "./../../models/Ticket";
 import { QUEUE_GROUP_NAME } from "./queue-group-name";
 
-export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+export class TicketUpdatedListener extends Listener<TicketCreatedEvent> {
   queueGroupName: string = QUEUE_GROUP_NAME;
   readonly subject = Subjects.TicketUpdated;
+
   async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     const ticket = await Ticket.findById(data.id);
     if (!ticket) throw new AppError("Ticket not found!", 404);
--- services/orders/src/index.ts
+++ services/orders/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { Stan } from "node-nats-streaming";
 import app from "./app";
+import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
+import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 import { natsWrapper } from "./nats-wrapper";
 
 let stan: Stan;
@@ -25,6 +27,9 @@ const start = async () => {
       console.log("NATS connection closed!");
       process.exit();
     });
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
