a64d3962b44cd8892f476b747c84e755655b8767
lschulzes <lucassilvaiee@gmail.com>
2022-09-04 20:49:29 -0300

    feat(services/tickets): emit TicketUpdated event inside order created listener

--- services/tickets/src/events/listeners/order-created-listener.ts
+++ services/tickets/src/events/listeners/order-created-listener.ts
@@ -1,6 +1,7 @@
 import { AppError, Listener, OrderCreatedEvent, Subjects } from "common";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/Ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 import { QUEUE_GROUP_NAME } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
@@ -11,8 +12,17 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     const ticket = await Ticket.findById(data.ticket.id);
 
     if (!ticket) throw new AppError("Ticket not found!", 404);
+    if (ticket.orderId) throw new AppError("Ticket already reserved", 422);
 
     await ticket.set({ orderId: data.id }).save();
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      version: ticket.version,
+    });
 
     msg.ack();
   }
